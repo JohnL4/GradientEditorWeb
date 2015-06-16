8ec62298af7992ec81f3270c1e8a18cbb572e155
John Lusk <john-git@how-hard-can-it-be.com>
2015-06-15 21:54:13 -0400

    Finished dynamically creating table rows onload.

--- gradient-data.ts
+++ gradient-data.ts
@@ -13,8 +13,8 @@ class GradientPoint
     }
 }
 
-var gradientPoints: GradientPoint[]
-{
+var gradientPoints: GradientPoint[] =
+[
     new GradientPoint( 0.0, "#660099"),
     new GradientPoint( 0.25, "#3366ff"),
     new GradientPoint( 0.5, "#cceeff"),
@@ -22,20 +22,90 @@ var gradientPoints: GradientPoint[]
     new GradientPoint( 0.675, "#00ff00"),
     new GradientPoint( 0.875, "#996633"),
     new GradientPoint( 1.0, "#ffffff")
-}
+];
 
 function loaded()
 {
-    var tbl : HTMLTableElement;
-    tbl = <HTMLTableElement> document.querySelector( "#gradientPointsTable");
+  clearTableRows( "gradientPointsTable");
+  addTableRows( "gradientPointsTable", gradientPoints);
+}
 
-    for (var i = 0; i < tbl.children.length; i++)
+function clearTableRows( aTableId: string)
+{
+  var tbl : HTMLTableElement;
+  tbl = <HTMLTableElement> document.querySelector( "#" + aTableId);
+
+  for (var i = 0; i < tbl.children.length; i++)
+  {
+    if (tbl.children[i].nodeType == Node.ELEMENT_NODE)
     {
-        var tbody : HTMLTableSectionElement = <HTMLTableSectionElement> tbl.children[i];
-        if (tbody.rows)
-        {
-            while (tbody.rows.length > 3)
-                tbody.deleteRow( 1);
-        }
+      // Assume first element node we hit is the table body (tbody).
+      var tbody : HTMLTableSectionElement = <HTMLTableSectionElement> tbl.children[i];
+      if (tbody.rows)
+      {
+          while (tbody.rows.length > 1)
+              tbody.deleteRow( 1);
+      }
+      break;
     }
+  }
+}
+
+function addTableRows( aTableId: string, aGradientPoints: GradientPoint[])
+{
+  var tbl : HTMLTableElement;
+  tbl = <HTMLTableElement> document.querySelector( "#" + aTableId);
+
+  for (var i = 0; i < tbl.children.length; i++)
+  {
+    if (tbl.children[i].nodeType == Node.ELEMENT_NODE)
+    {
+      // Assume first element node we hit is the table body (tbody).
+      var tbody : HTMLTableSectionElement = <HTMLTableSectionElement> tbl.children[i];
+      if (tbody.rows)
+      {
+        for (var j = 0; j < aGradientPoints.length; j++)
+        {
+          insertNewRow( tbody, aGradientPoints[j]);
+        }
+        insertLastEmptyRow( tbody);
+      }
+    }
+  }
+}
+
+function insertNewRow( aTBody: HTMLTableSectionElement, aGradientPoint: GradientPoint)
+{
+  var tr : HTMLTableRowElement = <HTMLTableRowElement> aTBody.insertRow();
+  var td : HTMLTableDataCellElement; // = <HTMLTableDataCellElement> tr.insertCell();
+
+  // Buttons
+  td = <HTMLTableDataCellElement> tr.insertCell( );
+  var button : HTMLButtonElement = <HTMLButtonElement> document.createElement( "BUTTON");
+  button.textContent = "+";
+  td.appendChild( button);
+  td = <HTMLTableDataCellElement> tr.insertCell( );
+  button = <HTMLButtonElement> document.createElement( "BUTTON");
+  button.textContent = "-";
+  td.appendChild( button);
+
+  // Position
+  td = <HTMLTableDataCellElement> tr.insertCell();
+  td.textContent = aGradientPoint.position.toString();
+
+  // Color
+  td = <HTMLTableDataCellElement> tr.insertCell();
+  td.textContent = aGradientPoint.color;
+}
+
+function insertLastEmptyRow( aTBody: HTMLTableSectionElement)
+{
+  var tr : HTMLTableRowElement = <HTMLTableRowElement> aTBody.insertRow();
+  var td : HTMLTableDataCellElement; // = <HTMLTableDataCellElement> tr.insertCell();
+
+  // Buttons
+  td = <HTMLTableDataCellElement> tr.insertCell( );
+  var button : HTMLButtonElement = <HTMLButtonElement> document.createElement( "BUTTON");
+  button.textContent = "+";
+  td.appendChild( button);
 }
